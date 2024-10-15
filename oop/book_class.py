570a29e06b3ce59ea91f3fb8ca177053cfb3c883
Mostafa Hegazy <mostafaa.hegazy47@gmail.com>
2024-10-15 23:50:15 +0300

    add book_class

---
+++ oop/book_class.py
@@ -0,0 +1,30 @@
+class Book:
+    def __init__(self, title:str, author:str, year:int):
+        self.title = title
+        self.author = author
+        self.year = year
+
+
+    def __del__(self):
+        print(f"deleting {self.title}")
+
+    def __str__(self):
+        print(f"{self.title} by {self.author} published in {self.year}")
+
+    def __repr__(self):
+        print(f"Book ('{self.title}), '{self.author}, '{self.year}")
+
+
+
+from book_class import Book
+
+def main():
+    my_book = Book("1984", "George Orwell", 1949)
+    print(my_book)  
+
+    print(repr(my_book))  
+
+    del my_book
+
+if __name__ == "__main__":
+    main()
